8e1bfbd732466489863f48a2ec251f8c08e9f333
ronen <narkisr@gmail.com>
2019-10-12 23:07:54 +1100

    moving to lein native image plugin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Intro
 
-This is a fork of [cljfmt-graalvm](https://gitlab.com/konrad.mrozek/cljfmt-graalvm) native binary version of cljfmt.
+This is a fork of [cljfmt-graalvm](https://gitlab.com/konrad.mrozek/cljfmt-graalvm) native binary version of cljfmt that uses stdin as input.
 
 ## Usage
 
@@ -15,10 +15,9 @@ $ cat src/foo.clj | cljfmt
 ```bash
 $ lein uberjar
 
-$ native-image -jar target/cljfmt-graalvm-0.1.0-standalone.jar -H:Name="cljfmt" --report-unsupported-elements-at-runtime --initialize-at-build-time
+$ lein native-image
 ```
 
 ## License
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- project.clj
+++ project.clj
@@ -1,10 +1,33 @@
 (defproject cljfmt-graalvm "0.1.0"
   :description "Clojure formatter using cljfmt built with GraalVM"
   :url "https://gitlab.com/konrad.mrozek/cljfmt-graalvm"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cljfmt "0.6.0"]]
+  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+      [org.clojure/clojure "1.8.0"]
+      [cljfmt "0.6.0"]
+  ]
+
+  :plugins [
+     [io.taylorwood/lein-native-image "0.3.1"]
+  ]
+
+  :profiles {
+      :uberjar {
+          :jvm-opts ["-Dclojure.compiler.direct-linking=true"] 
+          :aot :all
+      }
+  }
+
+
+  :native-image {
+      :name "cljfmt"
+      :graal-bin "/opt/graalvm-ce-19.2.0.1/bin/native-image"
+      :opts [
+        "--no-server"
+        "--report-unsupported-elements-at-runtime"
+        "--initialize-at-build-time"
+      ]
+  }
+
   :main cljfmt-graalvm.core
-  :profiles {:uberjar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
-                       :aot :all}})
+)
